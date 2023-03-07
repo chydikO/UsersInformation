1a68a422fb53d4f6371ab8d7b34fe021d77b80cc
chydikO_x220 <oleg.chudnovskiy.gmail.com>
2023-03-07 22:56:02 +0200

    add UI

---
+++ src/main/java/org/chudnovskiy0/Menu.java
@@ -0,0 +1,20 @@
+package org.chudnovskiy0;
+
+public enum Menu {
+    ADD("1. Добавить нового пользователя."),
+    DEL("2. Удалить существующего пользователя."),
+    IS_USER_PRESENT("3. Проверить существует ли пользователь."),
+    EDIT_LOGIN("4. Изменить логин существующего пользователя."),
+    EDIT_PASS("5. Изменить пароль существующего пользователя.");
+
+    public final String label;
+
+    Menu(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return this.label;
+    }
+}
