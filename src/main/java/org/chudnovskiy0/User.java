6a6ba3d910269e5a69783c97ebb54bc8fe06d234
chydikO_x220 <oleg.chudnovskiy.gmail.com>
2023-03-09 08:20:11 +0200

    x220 08-03-23

---
+++ src/main/java/org/chudnovskiy0/User.java
@@ -0,0 +1,13 @@
+package org.chudnovskiy0;
+
+import lombok.*;
+@RequiredArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+
+public class User {
+    private final String login;
+    private final String password;
+}
